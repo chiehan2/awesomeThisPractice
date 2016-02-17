4f7f8f357c43c62159e9cdeecfb93b2d189e81c2
chiehan2 <chieh.an.hsu2@gmail.com>
2016-02-17 17:37:37 +0800

    follow practice1, practice call and apply

---
+++ practices/practice2.js
@@ -0,0 +1,107 @@
+var dictionary = [
+  ['a', 'a\'s explanation'],
+  ['ab', 'ab\'s explanation'],
+  ['ac', 'ac\'s explanation'],
+  ['ad', 'ad\'s explanation'],
+  ['ae', 'ae\'s explanation'],
+  ['af ag', 'af ag\'s explanation'],
+  ['ah ai aj', 'ah ai aj\'s explanation'],
+  ['b', 'b\'s explanation'],
+  ['ba', 'ba\'s explanation'],
+  ['bb', 'bb\'s explanation'],
+  ['bc', 'bc\'s explanation'],
+  ['bd', 'bd\'s explanation'],
+  ['bf bg', 'bd\'s explanation'],
+  ['bh bi bj', 'bh bi bj\'s explanation']
+];
+
+function search (input) {
+  this.keyword = input;
+};
+
+search.prototype.matchWords = function (opt) {
+  var matchedWords = [];
+  var matchedIndexes = [];
+  if (opt === 'head') {
+  	var regex = new RegExp('^' + this.keyword);
+  };
+  if (opt === 'any') {
+  	var regex = new RegExp(this.keyword);
+  }; 
+  if (opt === 'whole') {
+  	var regex = new RegExp('^' + this.keyword + '$')
+  };
+  for (i = 0; i < dictionary.length; i ++) {
+  	var word = dictionary[i][0];
+  	var matchedWord = word.match(regex);
+  	if (matchedWord) {
+      matchedWords.push(word);
+      matchedIndexes.push(i);
+  	};
+  };
+  this.matchedWords = matchedWords;
+  this.matchedIndexes = matchedIndexes;
+  console.log(matchedWords);
+  return this;
+};
+
+search.prototype.showExplanation = function() {
+  if (!this.matchedIndexes) {
+  	console.log('not search yet');
+  	return this;
+  };
+  for (var i = 0; i < this.matchedIndexes.length; i ++) {
+  	var index = this.matchedIndexes[i];
+  	console.log(dictionary[index]);
+  };
+  return this;
+}
+
+var a = new search('a');
+var b = new search('b');
+
+a.matchWords('head').matchWords('whole').matchWords('any').showExplanation();
+b.showExplanation();
+
+///////////////////////    call and apply
+
+a.matchWords('any');
+
+function doubleSearch () {};
+
+doubleSearch.prototype.doubleMatchWords = function(secondKeyword) {
+  var doubleMatchedWords = [];
+  var reverseDoubleMatchIndexes = [];
+  var firstMatchedWords = this.matchedWords;
+  var regex = new RegExp(secondKeyword);
+  for (i = 0; i < firstMatchedWords.length; i ++) {    
+    var word = firstMatchedWords[i];
+    var doubleMatchedWord = word.match(regex);
+    if (doubleMatchedWord) {
+      doubleMatchedWords.push(word);
+      reverseDoubleMatchIndexes.push(i);
+    };
+  };
+  this.reverseDoubleMatchIndexes = reverseDoubleMatchIndexes;
+  console.log(doubleMatchedWords);
+  return this;
+};
+
+var ab = new doubleSearch();
+ab.doubleMatchWords.call(a, 'b');
+
+doubleSearch.prototype.explainReverseDoubleMatch = function(firstKeyword, secondKeyword) {
+  this.reverseDoubleMatchIndexes.sort(function(a, b) {
+    return b - a;
+  });
+  var reverseDoubleMatchWords = this.matchedWords;
+  for (var i = 0; i < this.reverseDoubleMatchIndexes.length; i ++) {
+    reverseDoubleMatchWords.splice(this.reverseDoubleMatchIndexes[i], 1);
+  };
+  console.log('The results of reverseDoubleMatchWords is : \n' 
+    + reverseDoubleMatchWords 
+    + ', \nit means the words in the dictionary which match ' + firstKeyword 
+    + ', but not match ' + secondKeyword + ' !');
+};
+
+ab.explainReverseDoubleMatch.apply(a, ['a', 'b']);
